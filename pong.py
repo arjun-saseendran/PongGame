2a7890a74a60c386e4a3406ca7af30b82f0ebacf
arjun-saseendran <arjunsaseendran@icloud.com>
2024-07-19 05:49:23 +0530

    version 1, window created, title added, background color set

---
+++ pong.py
@@ -0,0 +1,18 @@
+# pong game version 1
+
+# game window or screen
+import turtle as t
+
+wn = t.Screen()
+wn.title('Pong')
+wn.bgcolor('black')
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# main game loop
+while True:
+    wn.update()
+
+
+
+
